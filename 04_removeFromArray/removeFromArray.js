a3d3b69343f3a964b3e8211a5818e2a4626a59c5
Rowan Dowling <145838134+rowan-codes@users.noreply.github.com>
2025-02-19 21:57:27 +0000

    Added comments for readability

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
 const removeFromArray = function(array, ...itemsToRemove) {
     const newArray = [];
+    // for each item in the array argument, check against the itemsToRemove arguments
+    // if the item matches an itemToRemove, it doesn't get copied into the newArray, else it does
     array.forEach(item => {
         if (!itemsToRemove.includes(item)) {
             newArray.push(item);
