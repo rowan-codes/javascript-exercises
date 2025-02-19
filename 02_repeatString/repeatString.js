e313f5df893aa95e2bb0bbfcd0ebe3a62a2ecef6
Rowan Dowling <145838134+rowan-codes@users.noreply.github.com>
2025-02-19 19:52:33 +0000

    Solved exercises 01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(word, number_of_repeats) {
+    if (number_of_repeats < 0) return "ERROR";
+    let word_to_print = '';
+    for (let i = 0; i < number_of_repeats; i++) {
+        word_to_print += word
+    }
+    return word_to_print;
 };
 
 // Do not edit below this line
