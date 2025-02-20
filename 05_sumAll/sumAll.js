2bcdadda23de841fac939c318366d1fc2b245943
Rowan Dowling <145838134+rowan-codes@users.noreply.github.com>
2025-02-20 12:41:24 +0000

    Solved ex 05 with destructured array, integer and negative number checks, and for loop.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
+const sumAll = function(number1, number2) {
 
-};
+    //checks for expected number of arguments
+    if (arguments.length != 2) return "ERROR";
+
+    // checks for negative numbers, or if any arguments were not integers    
+    if (number1 < 0 || number2 < 0) return "ERROR";
+    if (!Number.isInteger(number1) || !Number.isInteger(number2)) return "ERROR";
+    
+    // sorts the numbers with a destructured array, swapping them if the first is larger than the second
+    if (number1 > number2) [number1, number2] = [number2, number1];
+
+    // calculates sum of all the integers between the two numbers, inclusive
+    let sum = 0;
+    for (let i = number1; i <= number2; i++){
+        sum += i;
+    }
+    return sum;
+    }
 
 // Do not edit below this line
 module.exports = sumAll;
