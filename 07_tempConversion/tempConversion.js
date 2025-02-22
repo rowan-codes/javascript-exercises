11a3dcc60570645abc50676d4f5462607830894e
Rowan Dowling <145838134+rowan-codes@users.noreply.github.com>
2025-02-22 14:04:44 +0000

    Solved ex 07 on temperature conversion F to C and C to F

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempFahrenheit) {
+  let convertedToCelsius = (tempFahrenheit - 32) * (5/9);
+  let roundedCelsius = Math.round(convertedToCelsius * 10) / 10;
+  return roundedCelsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempCelsius) {
+  let convertedToFahrenheit = tempCelsius * (9/5) + 32;
+  let roundedFahrenheit = Math.round(convertedToFahrenheit * 10) / 10;
+  return roundedFahrenheit;
 };
 
 // Do not edit below this line
