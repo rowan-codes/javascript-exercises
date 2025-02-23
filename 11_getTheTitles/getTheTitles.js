b3ba770094547bfa5cd0ff3aa3c942f06f591f89
Rowan Dowling <145838134+rowan-codes@users.noreply.github.com>
2025-02-23 13:43:03 +0000

    Solved ex 11 with array.map method

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    // working with an array of objects (JSON)
+    const titles = books.map((book) => book.title);
+    return titles;
 };
 
 // Do not edit below this line
