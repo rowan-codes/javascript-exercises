bf425235c05d4e947440208a150c83d2126ec3e8
Rowan Dowling <145838134+rowan-codes@users.noreply.github.com>
2025-02-23 12:16:17 +0000

    Solved 09 palindromes with .split .replace and .join methods

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (text) {
+    // remove punctuation then spaces
+    let punctuationless = text.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").toLowerCase();
+    let spaceless = punctuationless.replace(/\s+/g, '');
 
+    // split and reverse the string
+    let newText = spaceless.split("").join("");
+    let reversedText = spaceless.split("").reverse().join("");
+
+    // check equivalency
+    if (newText === reversedText) {return true;}
+    else return false;
 };
 
 // Do not edit below this line
